9fe92798a20e5df397688fc45892e821730c55f7
outofseason1 <jekafish228@mail.ru>
2023-11-15 21:37:36 +0500

    Добавление первого url, html, ico. Изменение файла README

--- mgsite/mgsite/asgi.py
+++ mgsite/mgsite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mathgenerator project.
+ASGI config for mgsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mathgenerator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mgsite.settings')
 
 application = get_asgi_application()
--- mgsite/mgsite/wsgi.py
+++ mgsite/mgsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mathgenerator project.
+WSGI config for mgsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mathgenerator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mgsite.settings')
 
 application = get_wsgi_application()
