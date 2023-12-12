0bc3671e43cbdd5f94f5f4973d77ad1c7c3ec72c
DuckStormGG <Ghost56YT@gmail.com>
2023-12-12 14:59:01 +0500

    добавил главную страницу, подключил css

--- mgsite/core/urls.py
+++ mgsite/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='home'),
+    path('login_test/', views.login_test)
 ]
--- mgsite/core/views.py
+++ mgsite/core/views.py
@@ -1,7 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template.loader import render_to_string
+
 
 def index(request):
-    return HttpResponse("Главная страница")
+    t = render_to_string('index.html')
+    return HttpResponse(t)
+
+
+@login_required()
+def login_test(request):
+    return HttpResponse("Закрытая страница")
 
 # Create your views here.
